b3999c5d83d373a1231648571115a2b392fab50a
Maximilian Kruse <maxkruse1996@gmail.com>
2022-05-30 00:06:38 +0200

    remove old containers in start script

--- start-docker.sh
+++ start-docker.sh
@@ -2,4 +2,7 @@
 
 docker build -t auctionbot:latest .
 
+docker stop auctionbot
+docker rm auctionbot
+
 docker run -v $(pwd):/usr/src/bot --restart=unless-stopped --name auctionbot -d auctionbot:latest
